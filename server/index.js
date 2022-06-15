7aee121ea4ec043c81022dabf7bc74844bb567cf
Selene Gabriela Amador Díaz <sel_nn@hotmail.com>
2022-06-15 16:27:55 -0500

    Upload image

--- server/index.js
+++ server/index.js
@@ -48,7 +48,7 @@ app.get('/api/config/paypal', (req, res) => {
     res.send(process.env.PAYPAL_CLIENT_ID || 'sb');
 });
 const __dirname = path.resolve();
-app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+app.use('/uploads', express.static(path.join(__dirname, '/uploads' || '/server/uploads')));
 app.use(express.static(path.join(__dirname, 'frontend/build')));
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'frontend/build/index.html')))
 // app.get('/', (req, res) => {
